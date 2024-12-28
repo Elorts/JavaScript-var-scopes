b1258ea779a4213aa800d5d0320556bec7569d66
deividas strole <deiwis@yahoo.com>
2024-12-27 19:12:06 -0800

    wip

--- starter/script.js
+++ starter/script.js
@@ -1,19 +1,38 @@
-'use strict';
+// 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
 
-  function printAge() {
-    const output = `You are ${age}, born in ${birthYear}`;
-    console.log(output);
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      const str = `You are millenial ${firstName}`;
-    }
-  }
+//   function printAge() {
+//     const output = `You are ${age}, born in ${birthYear}`;
+//     console.log(output);
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       const str = `You are millenial ${firstName}`;
+//     }
+//   }
 
-  printAge();
-  return age;
-}
+//   printAge();
+//   return age;
+// }
 
-const firstName = 'Jonas';
-calcAge(1991);
+// function aaa(a, b) {
+//   console.log('sadasasdas' + arguments);
+//   return a + b;
+// }
+
+// aaa(2, 5);
+// let xx;
+// const firstName = 'Jonas';
+// calcAge(1991);
+
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+
+console.log('Before', jessica);
+console.log('After:', marriedJessica);
